77398ec6a45c8aa718486542dc6ce9fcd27d8409
MizukiNishiyama <125176828+MizukiNishiyama@users.noreply.github.com>
2023-05-20 19:48:15 +0900

    poponn

--- hackathon_1/main.go
+++ hackathon_1/main.go
@@ -40,19 +40,25 @@ func main() {
 	closeDBWithSysCall()
 
 	log.Println("Listening...")
-	if err := http.ListenAndServe(":4000", nil); err != nil {
+	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func initDB() *sql.DB {
 	// DB接続のための準備
-	mysqlUser := os.Getenv("MYSQL_USER")
-	mysqlPwd := os.Getenv("MYSQL_PWD")
-	mysqlHost := os.Getenv("MYSQL_HOST")
-	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
+	//mysqlUser := os.Getenv("MYSQL_USER")
+	//mysqlPwd := os.Getenv("MYSQL_PWD")
+	//mysqlHost := os.Getenv("MYSQL_HOST")
+	//mysqlDatabase := os.Getenv("MYSQL_DATABASE")
 
-	connStr := fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	mysqlUser := "root"
+	mysqlPwd := "ramen102"
+	mysqlHost := "34.172.193.162:3306"
+	mysqlDatabase := "hackathon"
+
+	connStr := fmt.Sprintf("%s:%s@tcp(%s)/%s", mysqlUser, mysqlPwd, mysqlHost, mysqlDatabase)
+	fmt.Println(connStr)
 	_db, err := sql.Open("mysql", connStr)
 
 	if err != nil {
